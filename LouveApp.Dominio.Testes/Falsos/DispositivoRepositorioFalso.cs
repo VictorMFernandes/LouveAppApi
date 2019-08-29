8cf3119dd91597f548994ec0b90874c80b9bf9da
Victor Fernandes <victor.m.fernandes@outlook.com>
2019-08-28 22:45:43 -0300

    Criando testes para gerenciador de escalas

---
+++ LouveApp.Dominio.Testes/Falsos/DispositivoRepositorioFalso.cs
@@ -0,0 +1,14 @@
+﻿using LouveApp.Dominio.Repositorios;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LouveApp.Dominio.Testes.Falsos
+{
+    internal class DispositivoRepositorioFalso : IDispositivoRepositorio
+    {
+        public Task<IEnumerable<string>> PegarDispositivosTokensPorUsuarioId(List<string> usuariosIds)
+        {
+            return null;
+        }
+    }
+}
---
+++ LouveApp.Dominio.Testes/Gerenciadores/EscalaGerenciadorTestes.cs
@@ -0,0 +1,32 @@
+﻿using LouveApp.Dominio.Comandos.EscalaComandos.Entradas;
+using LouveApp.Dominio.Gerenciadores;
+using LouveApp.Dominio.Testes.Falsos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LouveApp.Dominio.Testes.Gerenciadores
+{
+    [TestClass]
+    public class EscalaGerenciadorTestes
+    {
+        private readonly EscalaGerenciador _gerenciador;
+
+        private readonly RegistrarEscalaComando _registrarEscalaComandoValido;
+
+        public EscalaGerenciadorTestes()
+        {
+            _gerenciador = new EscalaGerenciador(new MinisterioRepositorioFalso()
+                , new DispositivoRepositorioFalso(), new PushNotificationServicoFalso());
+
+            _registrarEscalaComandoValido = new RegistrarEscalaComando(DateTime.Now, null, null);
+        }
+
+        [TestMethod]
+        public void ValidarComandosAoExecutalos()
+        {
+            _ = _gerenciador.Executar(_registrarEscalaComandoValido);
+
+            Assert.IsTrue(_registrarEscalaComandoValido.FoiValidado);
+        }
+    }
+}
